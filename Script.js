a95f4160529f189d13fbaa57554afc546151a875
Victor Lucas <victorlucas2301@proton.me>
2024-04-16 20:18:58 -0300

    Changes to be committed: 	new file:   Header.css 	new file:   Menu/Favoritos/Favoritos.html 	new file:   Menu/Games/Games.css 	new file:   Menu/Games/Games.html 	new file:   Menu/Lobby/LOGOS/CS GO.png#	new file:   Menu/Lobby/LOGOS/FORTNITE.pn 	new file:   Menu/Lobby/LOGOS/ICONE GTA V.png#	new file:   Menu/Lobby/LOGOS/ICONE LOL.png 	new file:   Menu/Lobby/LOGOS/ICONE PUBG.png 	new file:   Menu/Lobby/LOGOS/MINECRAFT.png 	new file:   Menu/Lobby/LOGOS/Roblox-Logo.png 	new file:   Menu/Lobby/LOGOS/pokemon-go-png-logo-3164.png 	new file:   Menu/Lobby/Lobby.css 	new file:   Menu/Lobby/Lobby.html 	new file:   Menu/Login/Login.html 	new file:   Menu/Videos/Videos.html 	new file:   Script.js 	new file:   Wiki/Template/css/Template.css 	new file:   Wiki/Template/css/WikiDatabase.css 	new file:   Wiki/Template/html/Template.html 	new file:   Wiki/Template/html/WikiDatabase.html 	new file:   img/incial/Insta-icon.png 	modified:   img/incial/discord-icon.png 	modified:   index.html

---
+++ Script.js
@@ -0,0 +1,25 @@
+let isDown = false;
+let startX;
+let scrollLeft;
+
+const slider = document.querySelector ('.carrocel');
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX);
+  slider.scrollLeft = scrollLeft - walk;
+});
